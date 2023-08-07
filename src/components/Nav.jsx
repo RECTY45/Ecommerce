a1ee87343752379ca3223c32c475340596f4119c
RECTY <rectyexploit@gmail.com>
2023-08-07 15:12:08 +0800

    First Commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './nav.css'
+const Nav = () => {
+  return (
+    <div className="discount">
+      <p>Summer Sale For All Swim Suits And Free Express Delivery-OFF 50%!</p>
+    </div>
+  );
+}
+
+export default Nav;
