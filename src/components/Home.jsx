0730f5e8e1bb4499e240bbfe78cfe8c5bcec9743
RECTY <rectyexploit@gmail.com>
2023-08-24 15:36:17 +0800

    prosessing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import './home.css'
+import '/public/slider.png'
 const Home = () => {
     return (
-        <>
-           <div className="container">
-           <p>Woman's Fashion</p>
-           <p>Men's Fashion</p>
-           <p>Electronics</p>
-           <p>Home & Lifestyle</p>
-           <p>Medicine</p>
-           <p>Sports & Outdoor</p>
-           <p>Baby's & Toy's</p>
-           <p>Groceries & Pets</p>
-           <p>Health & Beauty</p>
-           </div>
-        </>
-    )
+      <>
+        <div className="container">
+          <div className="side">
+            <p>Woman's Fashion</p>
+            <p>Men's Fashion</p>
+            <p>Electronics</p>
+            <p>Home & Lifestyle</p>
+            <p>Medicine</p>
+            <p>Sports & Outdoor</p>
+            <p>Baby's & Toy's</p>
+            <p>Groceries & Pets</p>
+            <p>Health & Beauty</p>
+          </div>
+          <div className="slider">
+            <img src="slider.png" alt="avatar" />
+          </div>
+        </div>
+      </>
+    );
 }
 
 export default Home
